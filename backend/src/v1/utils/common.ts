4321853150ba3225540f67bf852de966ff467c07
varun9797 <verma9797@gmail.com>
2025-09-09 17:01:22 +0200

    not needed anymore

--- backend/src/v1/utils/common.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction, RequestHandler } from "express";
-
-export const asyncErrorHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>): RequestHandler => {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    }
-}
